25d62e6d59b7982bcb116902fa8b4454b52256a6
Angus Webb Hammond <anguswebbhammond@gmail.com>
2024-10-24 18:01:13 +0100

    updated sidebar styling

--- frontend/src/components/SidebarLink/index.tsx
+++ frontend/src/components/SidebarLink/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const SidebarLink = (props: Props) => {
   return (
-    <div className="flex h-12 justify-start items-center">
+    <div className="flex h-10 px-3 font-semibold text-lg justify-start items-center dark:bg-slate-700 dark:text-gray-300 rounded-full">
       <Link to={props.to}>{props.title}</Link>
     </div>
   )
