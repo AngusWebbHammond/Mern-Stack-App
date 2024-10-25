e790a57aff4dac2e287c464a2c1b80bb125fa95d
Angus Webb Hammond <anguswebbhammond@gmail.com>
2024-10-25 12:00:27 +0100

    improved sidebar button styling

--- frontend/src/components/SidebarLink/index.tsx
+++ frontend/src/components/SidebarLink/index.tsx
@@ -7,9 +7,11 @@ type Props = {
 
 const SidebarLink = (props: Props) => {
   return (
-    <div className="flex h-10 px-3 font-semibold text-lg justify-start items-center dark:bg-slate-700 dark:text-gray-300 rounded-full">
-      <Link to={props.to}>{props.title}</Link>
-    </div>
+    <Link 
+    to={props.to} 
+    className="flex h-10 px-3 font-semibold text-lg justify-start items-center dark:bg-slate-700 dark:text-gray-300 rounded-full hover:bg-slate-600">
+      {props.title}
+    </Link>
   )
 }
 
