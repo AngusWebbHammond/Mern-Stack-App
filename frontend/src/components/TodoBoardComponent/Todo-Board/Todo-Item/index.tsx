d138c5746b959bf3a57c0ad9289ffce89c31c4b8
Angus Webb Hammond <anguswebbhammond@gmail.com>
2024-10-26 14:26:52 +0100

    added deadline and priority updating

--- frontend/src/components/TodoBoardComponent/Todo-Board/Todo-Item/index.tsx
+++ frontend/src/components/TodoBoardComponent/Todo-Board/Todo-Item/index.tsx
@@ -1,7 +1,15 @@
-import { useState } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import { TodoType } from '../../../../types/todo-board-types';
 import DeleteButton from '../Delete-Button';
 import EditButton from '../Edit-Button';
+import DatePicker, { registerLocale, setDefaultLocale } from "react-datepicker";
+
+import { enGB } from 'date-fns/locale/en-GB';
+
+import "react-datepicker/dist/react-datepicker.css"
+
+registerLocale("en-GB", enGB)
+setDefaultLocale("en-GB")
 
 
 type Props = {
@@ -16,10 +24,14 @@ type Props = {
   setIsTitleUpdating: (isTitleUpdating: boolean) => void,
 }
 
-const TodoItem = (props: Props) => {
+const TodoItem = (props: Props) => {  
+  const deadline = new Date(props.todoItemDict.deadline);
+
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [tempTitle, setTempTitle] = useState<string>(props.todoItemDict.title);
   const [tempDescription, setTempDescription] = useState<string>(props.todoItemDict.description);
+  const [tempDeadline, setTempDeadline] = useState<Date | null>(deadline);
+  const [tempPriority, setTempPriority] = useState<string>(props.todoItemDict.priority)
 
   const updateTitle = (todoData: TodoType) => {
 
@@ -38,43 +50,83 @@ const TodoItem = (props: Props) => {
     <>
       <div 
         className={`bg-gray-300 dark:bg-slate-600 rounded-md gap-2 flex justify-between pl-3 pr-4 ring-2 ring-slate-700 py-3`}>
-          <div className='flex flex-col w-60 justify-center'>
+          <div className='flex flex-col w-60 justify-center gap-2'>
               {isEditing?<input 
                 className={props.h3TextStyling+' bg-gray-500 w-60'} 
                 type='text' 
                 autoFocus 
                 value={tempTitle} 
-                onChange={(e) => setTempTitle(e.currentTarget.value)} 
-                onKeyUp={(e) => {
-                  if (e.code === "Enter") {
-                    const tempItemData = props.todoItemDict;
-                    tempItemData.title = tempTitle;
-                    tempItemData.description = tempDescription;
-                    updateTitle(tempItemData);
-                  }}}/>
+                onChange={(e) => setTempTitle(e.currentTarget.value)}
+                />
               :<h3 className={props.h3TextStyling}>{props.todoItemDict.title}</h3>}
               {isEditing?<textarea 
                 className={'text-gray-700 dark:text-gray-300 font-normal text-sm bg-gray-500 w-60 min-h-24'} 
                 autoFocus
                 value={tempDescription}
-                onChange={(e) => setTempDescription(e.currentTarget.value)}
-                onKeyUp={(e) => {
-                  if (e.code === "Enter") {
-                    const tempItemData = props.todoItemDict;
-                    tempItemData.title = tempTitle;
-                    tempItemData.description = tempDescription;
-                    updateTitle(tempItemData);
-                  }}}/>
+                onChange={(e) => {
+                  setTempDescription(e.currentTarget.value)
+                }}
+                />
               :<h4 className={props.h4TextStyling}>{props.todoItemDict.description}</h4>}
+              {isEditing?
+              <div className="flex flex-row text-gray-700 dark:text-gray-300 gap-1">
+                <h5>Deadline:</h5>
+                <DatePicker 
+                selected={tempDeadline} 
+                onChange={(date) => setTempDeadline(date)}
+                withPortal
+                customInput={<button className="text-gray-700 dark:text-gray-300 font-normal text-sm bg-gray-500">{tempDeadline?.toLocaleDateString()}</button>}/>
+              </div>
+              :<h3 className={props.h4TextStyling}>{deadline.toLocaleDateString()}</h3>}
+
+              {isEditing?
+              <div className="flex flex-row text-gray-700 dark:text-gray-300 gap-1">
+                <h5>Priority:</h5>
+                <select className={`dark:bg-gray-500 
+                ${tempPriority === "Low"?
+                `dark:bg-green-600`:
+                tempPriority === "Medium"?
+                `dark:bg-yellow-600`:
+                `dark:bg-red-600`}`} value={tempPriority} onChange={(e) => setTempPriority(e.currentTarget.value)}>
+                  <option value="Low" className="bg-green-600">Low</option>
+                  <option value="Medium" className="bg-yellow-600">Medium</option>
+                  <option value="High" className="bg-red-600">High</option>
+                </select>
+              </div>
+              :<></>}
+              
+              {isEditing?
+              <div className="flex flex-row gap-2">
+                <button className="bg-slate-500 px-1 m-0 text-gray-700 dark:text-gray-200 font-semibold rounded-full"
+                onClick={() => {
+                  setIsEditing(false);
+                  setTempDeadline(deadline)
+                  setTempDescription(props.todoItemDict.description)
+                  setTempPriority(props.todoItemDict.priority)
+                  setTempTitle(props.todoItemDict.title)
+                }}>Cancel</button>
+                <button className="bg-slate-500 px-1 m-0 text-gray-700 dark:text-gray-200 font-semibold rounded-full"
+                onClick={() => {
+                  const tempItemData = props.todoItemDict;
+                  tempItemData.title = tempTitle;
+                  tempItemData.description = tempDescription;
+                  tempItemData.deadline = tempDeadline?.toDateString() as string;
+                  tempItemData.priority = tempPriority;
+                  console.log(tempPriority)
+                  updateTitle(tempItemData);
+                }}>Submit</button>
+              </div>
+              :<></>}
           </div>
           
           <div className='flex flex-col gap-2'>
             <div className='flex flex-col items-end gap-2'>
-              <h6 className={`${props.todoItemDict.priority === "Low"?
+              {isEditing?<></>:<h6 className={`${props.todoItemDict.priority === "Low"?
                 `bg-green-600`:
                 props.todoItemDict.priority === "Medium"?
                 `bg-yellow-600`:
-                `bg-red-600`} text-white rounded-full px-1 text-sm`}>{props.todoItemDict.priority}</h6>
+                `bg-red-600`} text-white rounded-full px-1 text-sm`}>{props.todoItemDict.priority}</h6>}
+              
               <div className='flex flex-row'>
                 <EditButton onClick={setIsEditing}/>
                 <DeleteButton item={props.todoItemDict._id} onClick={props.deleteTodoItem}/>
